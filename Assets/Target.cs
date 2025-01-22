d48a074c9eabb3f5eff08742b47e7c76acfa483c
Luke Cuzzetto <lacuzzetto@gmail.com>
2025-01-22 09:58:06 -0800

    Added Shooting Range and Logic

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private bool isHit = false; 
+    [SerializeField] public bool rangeTarget;
 
     public void HandleHit(Vector3 hitPoint, AudioClip hitSound)
     {
-        if (isHit) return; 
+        if(rangeTarget) {
+            AudioSource.PlayClipAtPoint(hitSound, hitPoint);
+        }
+
+        else if (isHit) {
+            return;
+        }
+
+        else if(!rangeTarget)
+        {
 
         isHit = true; 
         GameManager.Instance.UpdateScore(1); 
         AudioSource.PlayClipAtPoint(hitSound, hitPoint);
         Debug.Log("Target hit: " + gameObject.name);
 
+        }
     }
 }
